d965a7bf854552b3377d613204ed76e392b99516
Mary C <mcn4c@virginia.edu>
2021-04-20 14:17:23 -0400

    copy and pasted grid and jumbotron components into components file

---
+++ client/src/components/jumbotron.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Jumbotron({ children }) {
+  return (
+    <div
+
+      className="jumbotron"
+    >
+      {children}
+    </div>
+  );
+}
+
+export default Jumbotron;
