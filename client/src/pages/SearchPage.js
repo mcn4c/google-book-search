183341855df662e6fe38d2e2e20d40256f3126d7
Mary C <mcn4c@virginia.edu>
2021-04-20 11:25:00 -0400

    added some functionality to SearchPage.js file

--- client/src/pages/SearchPage.js
+++ client/src/pages/SearchPage.js
@@ -1,1 +1,49 @@
 import React, { useState } from "react";
+import Search from "../components/Search";
+import Button from "../components/Button";
+import API from "../utils/api";
+
+
+function SearchPage() {
+
+
+    const [searchValue, setSearchValue] = useState("")
+
+    //captures user input
+    const handleInputChange = e => {
+        const { value } = e.target;
+        setSearchValue(value);
+    };
+    //on submit makes api call
+    const handleFormSubmit = event => {
+        event.preventDefault();
+        API.getbooks(searchValue)
+            .then(data => console.log(data))
+            .catch(error => alert(error));
+    };
+
+
+    return (
+
+
+
+
+
+
+<form>
+<Container>
+    <Row>
+        <Col size="xs-9 sm-10">
+            <Search 
+            onChange={handleInputChange}/>
+        </Col>
+        <Col size="xs-3 sm-2">
+            <Button onClick={handleFormSubmit}>Search</Button>
+        </Col>
+    </Row>
+</Container>
+</form>
+    )
+}
+
+export default SearchPage;
