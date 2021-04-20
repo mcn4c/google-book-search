3651d06ede9588933b4e2daeec79a9e8c876ae94
Mary C <mcn4c@virginia.edu>
2021-04-20 12:49:59 -0400

    created server.js file

---
+++ client/src/components/Saved.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Saved({ children }) {
+    return (
+        <div className="saved-books">
+            {children}
+        </div>
+    )
+}
+
+export default Saved;
